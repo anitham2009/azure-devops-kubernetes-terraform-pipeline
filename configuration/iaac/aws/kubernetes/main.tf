cc130baa04fd1ad0f96065fdc5c3d34e36d1aaf2
anitham2009 <anitham2009@gmail.com>
2023-04-11 19:30:19 +0530

    Updated module version

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -31,6 +31,7 @@ provider "kubernetes" {
 
 module "anitha-cluster" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
   cluster_name    = "anitha-cluster"
   cluster_version = "1.17"
   subnets         = ["subnet-0b900a5420bbd4acf", "subnet-0d691af63605263b9", "subnet-09f0ff785ea9fbc41"] #CHANGE # Donot choose subnet from us-east-1e
